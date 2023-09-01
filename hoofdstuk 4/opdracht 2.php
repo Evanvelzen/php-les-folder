54dfddd21e65df6bf53e2f94483e14e57a9282a7
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-09-01 11:51:31 +0200

    opdracht 2 gedaan

--- hoofdstuk 4/opdracht 2.php
+++ hoofdstuk 4/opdracht 2.php
@@ -1,2 +1,11 @@
-<php
+<?php
+    $time = 9;
 
+    $result = match($time) {
+        6, 7, 8, 9, 11, 12 => "ochtend",
+        13, 14, 15, 17, 18 => "middag",
+        19, 20, 21, 22, 23, 24 => "avond",
+        0, 1, 2, 3, 4, 5 => "nacht",
+    };
+
+    echo $result;
