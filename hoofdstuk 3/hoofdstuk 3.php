57cc188b7cf1291250b5c1312efc1339e5eabbd9
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-09-01 11:20:44 +0200

    hoofdstuk 3 gedaan en begonnen aan hoofdstuk 4

---
+++ hoofdstuk 3/hoofdstuk 3.php
@@ -0,0 +1,26 @@
+<?php
+//variablelen
+    $date = date("l-j-F-Y");
+    $date2 = date("z");
+    $date3 = date("l");
+    $date4 = date("w");
+    $date5 = date("t");
+    $date6 = date("L");
+    $check = 1;
+
+//hier wordt er een e bij de datum gezet indien nodig
+    $date4 = $date4 . "e";
+    $date2 = $date2 . "e";
+
+//hier wordt code geprint op het scherm
+echo "Het is vandaag: $date. <br>";
+echo "Vandaag is het de $date2 dag van het jaar. <br>";
+echo "$date3 is de $date4 dag van de week. <br>";
+echo "De maand $date3 heeft in totaal $date5 dagen. <br>";
+
+//else if statement
+if($date6 < $check) {
+    echo "Er is geen schikkeljaar op het moment. <br>";
+    } else {
+    echo "Er is een schikkeljaar op het moment. <br>";
+}
---
+++ hoofdstuk 4/opdracht1.php
@@ -0,0 +1,13 @@
+<?php
+
+$time = 13;
+
+if($time > 6 && $time < 12 ) {
+    echo "het is ochtend jij kut";
+} else if ($time > 12 && $time < 18){
+    echo "het is middag jij luie donder";
+} else if($time > 18 && $time< 24){
+    echo "het is avond";
+} else if($time > 0 && $time < 6){
+    echo "het is nacht ";
+};
