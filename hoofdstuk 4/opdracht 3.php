97fffbebd73239e754a5bc14c3784a693a9b31ea
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-09-01 12:32:32 +0200

    opdracht 3 bezig

---
+++ hoofdstuk 4/opdracht 3.php
@@ -0,0 +1,17 @@
+<?php
+    $numberOne = 1944;
+    $numberTwo =  1959;
+    //bonus punten voor als iemand weet wat er zo bijzonder is aan deze cijfers
+
+    if($numberOne > $numberTwo){
+        $tempresult = $numberOne * 2;
+        $result = $tempresult + $numberTwo;
+
+        echo $result;
+    } elseif ($numberTwo < $numberOne){
+        $tempresult = $numberTwo * 2;
+        $result = $tempresult + $numberOne;
+
+        echo $result;
+    }
+
