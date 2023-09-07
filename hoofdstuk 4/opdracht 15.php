b77fbd2548c3a88c15666817ad8eb4cda091acb9
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-09-07 11:44:23 +0200

    klaar ish
    ben over het algemeen klaar met de opdrachten enkelt blijf ik steken opdracht 9

--- hoofdstuk 4/opdracht 15.php
+++ hoofdstuk 4/opdracht 15.php
@@ -1,1 +1,26 @@
 <?php
+
+for ($i = 1; $i <= 1; $i++){
+    echo "$i ";
+}
+echo "<br>";
+for ($i = 1; $i <= 2; $i++){
+    echo "$i ";
+}
+echo "<br>";
+for ($i = 1; $i <= 3; $i++){
+    echo "$i ";
+}
+echo "<br>";
+for ($i = 1; $i <= 4; $i++){
+    echo "$i ";
+}
+echo "<br>";
+for ($i = 1; $i <= 5; $i++){
+    echo "$i ";
+}
+echo "<br>";
+for ($i = 1; $i <= 6; $i++){
+    echo "$i ";
+}
+
