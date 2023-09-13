24cb091a826c233e64a04ee0dc05619114559bf2
Evanvelzen <112620558+Evanvelzen@users.noreply.github.com>
2023-09-13 12:47:22 +0200

    opdracht 5 en 6 gedaan

--- hoofdstuk 6/opdracht 5.php
+++ hoofdstuk 6/opdracht 5.php
@@ -1,2 +1,23 @@
 <?php
 
+function calculatebrake($brakeLength){
+
+    $brakeDelay = 5.5;
+
+
+
+    $carrot1 = sqrt(2 );
+    $carrot2 = sqrt($brakeDelay);
+    $carrot3 =  sqrt($brakeLength);
+
+    $speed = $carrot1 * $carrot2 * $carrot3;
+
+    $speed = $speed * 3.6;
+
+    $speed = number_format($speed, decimals: 0);
+
+    echo "De snelheid van de auto met remspoor :$brakeLength M is $speed KM/U";
+}
+
+calculatebrake(50);
+
--- hoofdstuk 6/opdracht 6.php
+++ hoofdstuk 6/opdracht 6.php
@@ -1,6 +1,13 @@
 <?php
-$answer = 0;
-function calculation($answer){
 
+function calculate($dollar)
+{
+    $euro = $dollar * 0.93;
+
+    $euro = number_format($euro, decimals: 2 );
+    echo "$$dollar is gelijk €$euro";
 }
+
+calculate(86.74);
+
 
